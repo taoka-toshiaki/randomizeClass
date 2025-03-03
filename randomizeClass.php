9a6280557891bcec6308fd12ce15b82957e25f06
taoka-toshiaki <71550806+taoka-toshiaki@users.noreply.github.com>
2025-03-04 06:45:40 +0900

    Update randomizeClass.php

--- randomizeClass.php
+++ randomizeClass.php
@@ -1,66 +1,57 @@
 <?php
-// 頻繁にランダム番号が重複しないようにするクラス.
+// 頻繁にランダム番号が重複しないようにするクラス
 class randomizeClass
 {
     /**
      * ランダムに配列のインデックス番号を取得する
      */
-    public function getRandomIndex($hasArray,$index,$filename='rand.dat',$max=15)
+    public function getRandomIndex(array $array, int $index, string $filename = 'rand.dat', int $max = 15): int
     {
         try {
-
-            //過去のランダム番号を保存しているファイルが無ければ空ファイルを作成する
-            if(!file_exists($filename)){                
-                file_put_contents($filename,'');
+            // 過去のランダム番号を保存しているファイルが無ければ空ファイルを作成する
+            if (!file_exists($filename)) {
+                file_put_contents($filename, '');
             }
 
-            //過去のランダム番号を保存しているデータを取得
+            // 過去のランダム番号を取得
             $fileData = file_get_contents($filename);
 
-            //下記の場合は処理しない
-            if ($fileData === false || count($hasArray) < $max) {
+            // データ取得失敗や配列サイズが max より小さい場合はそのまま返す
+            if ($fileData === false || count($array) < $max) {
                 return $index;
             }
-            //ファイルデータをカンマで配列に分離
-            $isArray = explode(',',$fileData);
 
-            //過去のランダム番号に存在しないか？
-            if (array_search($index,$isArray,false) === false) {
-                //ランダム格納前処理
-                array_unshift($isArray,$index);
-                if (count($isArray)>$max) {
-                    array_pop($isArray);
+            // カンマ区切りで配列化し、空文字を取り除く
+            $history = array_filter(explode(',', $fileData), fn($val) => $val !== '');
+
+            // 過去のランダム番号に存在しない場合
+            if (!in_array($index, $history, false)) {
+                // 履歴の先頭に追加し、最大数を超えたら削除
+                array_unshift($history, $index);
+                if (count($history) > $max) {
+                    array_pop($history);
                 }
-                array_filter($isArray,function($val){
-                    return $val !=='';
-                });
-                //ランダム番号を格納
-                file_put_contents($filename,implode(',',$isArray));
+
+                // 履歴を保存
+                file_put_contents($filename, implode(',', $history));
 
                 return $index;
             }
-            //過去のランダム番号に合致したため再帰処理を行う．
-            return $this->getRandomIndex($hasArray,array_rand($hasArray),$filename,$max);
 
+            // 重複していた場合、新しいランダム値を再取得（再帰処理）
+            return $this->getRandomIndex($array, array_rand($array, 1), $filename, $max);
         } catch (\Throwable $th) {
             throw $th;
-            echo $th->getMessage();
         }
     }
 }
 
-//テスト用配列を生成
-$hasArray = (function(){
-    $val = [];
-    for($i=0;$i<100;$i++){
-        $val[] = $i;
-    }
-    return $val;
-})();
+// テスト用配列を生成
+$array = range(0, 99);
 $filename = 'rand.dat';
-//ランダム番号::配列インデックスを取得し表示
-print (new randomizeClass)->getRandomIndex($hasArray,array_rand($hasArray),$filename);
-print PHP_EOL;
-//過去のランダム番号を保存しているデータを取得し表示
-print file_get_contents($filename);
-print PHP_EOL;
+
+// ランダム番号::配列インデックスを取得し表示
+echo (new randomizeClass())->getRandomIndex($array, array_rand($array, 1), $filename) . PHP_EOL;
+
+// 過去のランダム番号を保存しているデータを取得し表示
+echo file_get_contents($filename) . PHP_EOL;
